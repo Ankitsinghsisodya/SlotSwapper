bb8d96637a9f72b653e69fd902dd19322b30b8c5
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-11-05 17:59:20 +0530

    feat: add ProtectedRoute component to secure access to the Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import SigninPage from './pages/LoginPage'
+import ProtectedRoute from './components/ProtectedRoute'
 import AuthCallback from './pages/AuthCallback'
 import Dashboard from './pages/Dashboard'
+import SigninPage from './pages/LoginPage'
 import SignupPage from './pages/SignUpPage'
 
 function App() {
@@ -12,22 +13,26 @@ function App() {
 
   return (
     <>
-<BrowserRouter>
-      <Routes>
-        {
-        <Route path="/" element={<Dashboard />} /> }
-        <Route path="/login" element={<SigninPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        
-        {/* 👇 This is the crucial part 👇 */}
-        <Route 
-          path="/auth/google/callback" 
-          element={<AuthCallback />} 
-        />
-        
-        {/* Other routes... */}
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {
+            <Route path="/" element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />}
+          <Route path="/login" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* 👇 This is the crucial part 👇 */}
+          <Route
+            path="/auth/google/callback"
+            element={<AuthCallback />}
+          />
+
+          {/* Other routes... */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
